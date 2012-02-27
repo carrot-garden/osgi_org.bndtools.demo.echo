89ba96880d529570c90357b25a7ee56ba5ed8ec0
developer <develop@pro-pix.de>
2012-02-27 21:48:48 +0100

    add groovyx service handling in echo client.

--- org.bndtools.demo.echo/src/org/bndtools/demo/echo/client/EchoClient.groovy
+++ org.bndtools.demo.echo/src/org/bndtools/demo/echo/client/EchoClient.groovy
@@ -1,31 +1,35 @@
 package org.bndtools.demo.echo.client
 
+import groovyx.osgi.OsgiCategory;
+
 import org.bndtools.demo.echo.api.Echo
+import org.osgi.framework.BundleContext;
 
 import aQute.bnd.annotation.component.*
+
 
 @Component(provide=[])
 class EchoClient {
 
-	private Echo echo
-
 	@Reference
-	void setEcho(Echo echo) {
-		this.echo = echo
-	}
-	
-	void unsetEcho(Echo echo) {
-		this.echo = null
-	}
-	
+	void setEcho(Echo echo) {}
+		
 	@Activate
-	void start() {
-		echo.show "Echo Client is started."
+	void start(BundleContext context) {
+		use(OsgiCategory) {
+			context.findServices(Echo).withEachService() { echo ->
+				echo.show('Start client get echo with groovyx-osgi')
+			}
+		}
 	}
 	
 	@Deactivate
-	void stop() {
-		echo.show "Stop the Echo Client."
+	void stop(BundleContext context) {
+		use(OsgiCategory) {
+			context.findServices(Echo).withEachService() { echo ->
+				echo.show('Stop client get echo with groovyx-osgi')
+			}
+		}
 	}
 	
 }
